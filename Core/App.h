967eb8fd38aa66a2454114dab93fb3a541a648cf
Veronika <127787290+VeronikaVovkCode@users.noreply.github.com>
2023-12-17 13:33:46 +0200

    Update App.h

--- Core/App.h
+++ Core/App.h
@@ -1,1 +1,25 @@
+#pragma once
+#include "Window.h"
+#include "Renderer.h"
+#include "../Utility/FileSystem.h"
+#include "../Game/TextureAtlas.h"
+#include "../Game/Level.h"
+#include "../UI/UI.h"
 
+class App
+{
+public:
+    App();
+    ~App();
+    int Go();
+private:
+    void DoFrame(float timestep);
+    void HandleInput(float timestep);
+private:
+    Window m_wnd;
+    Renderer m_renderer;
+    FileSystem& m_filesystem;
+    TextureAtlas& m_atlas;
+    Level m_level;
+    UI m_ui;
+};
